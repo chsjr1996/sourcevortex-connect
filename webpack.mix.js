2b3533a7e469d8583b1eb8a4e5903edff9f9be41
Carlos Henrique <chsjr1996@gmail.com>
2021-09-18 13:18:33 -0300

    Add Laravel Breeze
    - Add BrowserSync
    - Add Inertia.JS (with VueJS 3)

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,33 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .vue()
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .webpackConfig(require('./webpack.config'));
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.browserSync({
+    host: '127.0.0.1',
+    proxy: 'localhost',
+    open: false,
+    notify: false,
+    files: [
+        'app/**/*.php',
+        'resources/views/**/*.php',
+        'public/js/**/*.js',
+        'public/css/**/*.css'
+    ],
+    watchOptions: {
+        usePolling: true,
+        interval: 500
+    }
+});
+
+mix.disableNotifications();
